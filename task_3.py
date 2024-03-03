c488a1f8b718dba49dcfa4d28638b83545d36882
AESolodchuk <solodchuk.aleksey@gmail.com>
2024-03-03 15:54:18 +0200

    final

--- task_3.py
+++ task_3.py
@@ -2,56 +2,68 @@ from task_3_algorithms import boyer_moore, knut_morris, rabin_karp
 import timeit
 import requests
 
-first_text = requests.get("https://drive.google.com/uc?id=18_R5vEQ3eDuy2VdV3K5Lu-R-B-adxXZh").text
-second_text = requests.get("https://drive.google.com/uc?id=13hSt4JkJc11nckZZz2yoFHYL89a4XkMZ").text
+first_text = requests.get(
+    "https://drive.google.com/uc?id=18_R5vEQ3eDuy2VdV3K5Lu-R-B-adxXZh"
+).text
+second_text = requests.get(
+    "https://drive.google.com/uc?id=13hSt4JkJc11nckZZz2yoFHYL89a4XkMZ"
+).text
 
 
+print(first_text)
 phraze_to_find_first_text = "Пошук – поширена дія, яка виконується в бізнес-додатках"
 phraze_to_find_second_text = "Випадковим чином обирається контрольна сесія"
 phrazae_to_find_improvization = "щось таке чого нема у тексті"
 
-#Пошук у першому тексті
+
+# Пошук у першому тексті
 te_search_boyer_moore_first_text = timeit.timeit(
-    lambda: boyer_moore.boyer_moore_search(first_text, phraze_to_find_first_text), number=5
+    lambda: boyer_moore.boyer_moore_search(first_text, phraze_to_find_first_text),
+    number=5,
 )
-te_search_knut_morris_first_text  = timeit.timeit(
+te_search_knut_morris_first_text = timeit.timeit(
     lambda: knut_morris.kmp_search(first_text, phraze_to_find_first_text), number=5
 )
-te_search_rabin_karp_first_text  = timeit.timeit(
-    lambda: rabin_karp.rabin_karp_search(first_text, phraze_to_find_first_text), number=5
+te_search_rabin_karp_first_text = timeit.timeit(
+    lambda: rabin_karp.rabin_karp_search(first_text, phraze_to_find_first_text),
+    number=5,
 )
 
-ti_search_boyer_moore_first_text  = timeit.timeit(
+ti_search_boyer_moore_first_text = timeit.timeit(
     lambda: boyer_moore.boyer_moore_search(first_text, phrazae_to_find_improvization),
     number=5,
 )
-ti_search_knut_morris_first_text  = timeit.timeit(
+ti_search_knut_morris_first_text = timeit.timeit(
     lambda: knut_morris.kmp_search(first_text, phrazae_to_find_improvization), number=5
 )
-ti_search_rabin_karp_first_text  = timeit.timeit(
-    lambda: rabin_karp.rabin_karp_search(first_text, phrazae_to_find_improvization), number=5
+ti_search_rabin_karp_first_text = timeit.timeit(
+    lambda: rabin_karp.rabin_karp_search(first_text, phrazae_to_find_improvization),
+    number=5,
 )
 
-#Пошук у другому тексті
+# Пошук у другому тексті
 te_search_boyer_moore_second_text = timeit.timeit(
-    lambda: boyer_moore.boyer_moore_search(second_text, phraze_to_find_second_text), number=5
-)
-te_search_knut_morris_second_text  = timeit.timeit(
-    lambda: knut_morris.kmp_search(second_text, phraze_to_find_second_text), number=5
-)
-te_search_rabin_karp_second_text = timeit.timeit(
-    lambda: rabin_karp.rabin_karp_search(second_text, phraze_to_find_second_text), number=5
-)
-
-ti_search_boyer_moore_second_text  = timeit.timeit(
     lambda: boyer_moore.boyer_moore_search(second_text, phraze_to_find_second_text),
     number=5,
 )
-ti_search_knut_morris_second_text  = timeit.timeit(
+te_search_knut_morris_second_text = timeit.timeit(
     lambda: knut_morris.kmp_search(second_text, phraze_to_find_second_text), number=5
 )
-ti_search_rabin_karp_second_text  = timeit.timeit(
-    lambda: rabin_karp.rabin_karp_search(second_text, phraze_to_find_second_text), number=5
+te_search_rabin_karp_second_text = timeit.timeit(
+    lambda: rabin_karp.rabin_karp_search(second_text, phraze_to_find_second_text),
+    number=5,
+)
+
+ti_search_boyer_moore_second_text = timeit.timeit(
+    lambda: boyer_moore.boyer_moore_search(second_text, phraze_to_find_second_text),
+    number=5,
+)
+ti_search_knut_morris_second_text = timeit.timeit(
+    lambda: knut_morris.kmp_search(second_text, phraze_to_find_second_text), number=5
+)
+ti_search_rabin_karp_second_text = timeit.timeit(
+    lambda: rabin_karp.rabin_karp_search(second_text, phraze_to_find_second_text),
+    number=5,
 )
 
 
